cdb96c924b1ba50188a90a6bb519e1527d5402d0
laskar-ksatria <dock_fest@yahoo.co.id>
2021-01-27 16:45:30 +0700

    add

--- pages/graphqlsample.js
+++ pages/graphqlsample.js
@@ -4,11 +4,6 @@ function GraphSample() {
     return (
        <Layout>
            <h1>GraphSample</h1>
-           <p>Halol</p>
-           <p>Halol</p>
-           <p>Halol</p>
-           <p>Halol</p>
-           <p>Halol</p>
        </Layout>
     )
 }
